a4e7e06e9c4ce9a81deca690700c17c042865087
Veshal Raj <veshalraj1307@gmail.com>
2024-01-13 22:00:29 +0530

    fetch the first movie data

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,11 @@
 export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 
 export const USER_AVATAR = "https://avatars.githubusercontent.com/u/115520285?v=4"
+
+export const API_OPTIONS ={
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NjkwMTQxZWJmMzM1ZTIwNmJkYzk4OWRjM2Y0M2VlNCIsInN1YiI6IjY1YTJiMzlkMjY2Nzc4MDEyODY0MTVkNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.g77ZXoKmnr1b2fxH7jp0NjdhVAdejOYYPg-9zRIMR2Q'
+    }
+  };
